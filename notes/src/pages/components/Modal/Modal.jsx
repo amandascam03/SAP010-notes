9da605487b86f9dc074714aa3219d903eba0c24a
amandascam03 <amandascam03@gmail.com>
2023-09-22 16:55:26 -0300

    adicionando opção de deletar nota com deleteDoc e filter

--- notes/src/pages/components/Modal/Modal.jsx
+++ notes/src/pages/components/Modal/Modal.jsx
@@ -1,13 +1,20 @@
-export function Modal({title, description, close}) {
+export function Modal({title, description, close, deleteOption}) {
+
+    function handleClick() {
+        deleteOption();
+        close();
+    }
+
     return (
         <div className="bgModal">
             <div className="modal">
                 <button className="returnBtn" onClick={close}></button>
                 <h2 className="titleModal">{title}</h2>
                 <p className="descModal">{description}</p>
+                <hr />
                 <div className="optionsBtn">
                 <button>Editar</button>
-                <button>Excluir</button>
+                <button onClick={handleClick}>Excluir</button>
                 </div>
             </div>
         </div>
--- notes/src/pages/home/Home.jsx
+++ notes/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthGoogleContext } from "../../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
 import { auth, db } from "../../lib/firebase-config";
 import { Modal } from "../components/Modal/Modal";
 
@@ -11,6 +11,7 @@ export default function HomePage() {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [modalContent, setModalContent] = useState({});
     const navigate = useNavigate();
+    const userSigned = JSON.parse(user);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (authObj) => {
@@ -32,14 +33,17 @@ export default function HomePage() {
                 console.log(notesData);
 
                 setNotes(notesData);
-
             } else {
                 console.log("usuário não logado");
             }
         });
     }, []);
 
-    const userSigned = JSON.parse(user);
+    async function deleteNote(noteId) {
+        await deleteDoc(doc(db, "notes", noteId));
+        const newNotes = notes.filter((n) => n.id !== noteId);
+        setNotes(newNotes);
+    }
 
     return (
         <div className="HomePage">
@@ -52,15 +56,27 @@ export default function HomePage() {
             </header>
             <div className="notes">
                 {notes.map((note) => (
-                    <div key={note.id} className="note" onClick={() => {setModalIsOpen(true)
-                    setModalContent(note)
-                    }}>
+                    <div
+                        key={note.id}
+                        className="note"
+                        onClick={() => {
+                            setModalIsOpen(true);
+                            setModalContent(note);
+                        }}
+                    >
                         <h1>{note.titulo}</h1>
                         <p>{note.conteudo}</p>
                     </div>
                 ))}
             </div>
-            {modalIsOpen && <Modal title={modalContent.titulo} description={modalContent.conteudo} close={() => setModalIsOpen(false)} />}
+            {modalIsOpen && (
+                <Modal
+                    title={modalContent.titulo}
+                    description={modalContent.conteudo}
+                    close={() => setModalIsOpen(false)}
+                    deleteOption={() => deleteNote(modalContent.id)}
+                />
+            )}
             <button
                 className="addBtn"
                 title="Add"
